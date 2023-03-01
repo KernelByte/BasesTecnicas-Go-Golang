7630e991720a07e10764f5fc4bfaf48b2eab9295
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-03-01 07:56:25 -0500

    Clase #2 sobre los tipos de variables y sus valores por defecto en go

--- src/main.go
+++ src/main.go
@@ -7,5 +7,29 @@ import (
 
 // Funcion principal Main
 func main() {
-	fmt.Println("Hello World")
+
+	// Declaracion de una variable constante
+	const variable float64 = 3.14
+	const variable2 = 3.14345
+	fmt.Println("El valor de la variables es: ", variable, "Y el valor de la segunda es: ", variable2)
+
+	// Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Print(base, altura, area, "/")
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// realizar el area de un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("El area es: ", areaCuadrado)
 }
